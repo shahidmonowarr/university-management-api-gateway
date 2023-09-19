8b1792ca48b53973978ecfc4847a97140aeb24e2
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-19 19:52:25 +0600

    single academic semester get, update & delete

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -3,6 +3,10 @@ import { academicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
 
+router.get('/', academicSemesterController.getAllFromDB);
+router.get('/:id', academicSemesterController.getOneFromDB);
 router.post('/', academicSemesterController.insertIntoDB);
+router.patch('/:id', academicSemesterController.updateOneIntoDB);
+router.delete('/:id', academicSemesterController.deleteOneFromDB);
 
 export const academicSemesterRoutes = router;
