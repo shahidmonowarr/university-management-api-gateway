5dab64b33f55bb632bbccb97c6095f46aa2ef4e7
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-20 23:36:21 +0600

    api gateway for course, offeredCourse, offeredCourseSection & semester reg

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,7 +3,11 @@ import { academicDepartmentRoutes } from '../modules/academicDepartment/academic
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { buildingRoutes } from '../modules/building/building.routes';
+import { courseRoutes } from '../modules/course/course.routes';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.routes';
+import { offeredCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.routes';
 import { roomRoutes } from '../modules/room/room.routes';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
 
 const router = express.Router();
 
@@ -27,6 +31,22 @@ const moduleRoutes = [
   {
     path: '/rooms',
     routes: roomRoutes
+  },
+  {
+    path: '/courses',
+    routes: courseRoutes
+  },
+  {
+    path: '/semester-registrations',
+    routes: semesterRegistrationRoutes
+  },
+  {
+    path: '/offered-courses',
+    routes: offeredCourseRoutes
+  },
+  {
+    path: '/offered-course-sections',
+    routes: offeredCourseSectionRoutes
   }
 ];
 
